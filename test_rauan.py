ee0a085aff3792b7c7a63376a72cf6ce8e6ae1e1
galikhan <galihan.abdikarim@gmail.com>
2022-08-27 17:08:26 +0600

    fourth try

--- test_rauan.py
+++ test_rauan.py
@@ -1,15 +1,15 @@
 def reverseWords():
     while True:
         nr = str(input())
-
         int_list = [int(i) for i in nr if i.isdigit()]
-        if len(int_list) > 1:
+        if len(int_list) >= 1:
             print('error, rewrite!')
         else:
             reverse = nr[::-1]
+            return reverse
+        if nr == '':
             break
 
-    return reverse
 
 pr = reverseWords()
 print(pr)
